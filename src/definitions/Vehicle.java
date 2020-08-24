9a39c32a89fdf00a762db1668f09b502fba38846
dbc2201 <divyanshb22@gmail.com>
2020-08-24 15:04:28 +0530

    create Vehicle.java class

---
+++ src/definitions/Vehicle.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 24/08/20
+ *  Time: 3:04 PM
+ *  File Name : Vehicle.java
+ * */
+package definitions;
+
+public class Vehicle {
+}
