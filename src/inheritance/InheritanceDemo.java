600b259bf67e1d72bee0f61a612a17b2cf9420f2
dbc2201 <divyanshb22@gmail.com>
2020-08-24 10:05:17 +0530

    initial commit

---
+++ src/inheritance/InheritanceDemo.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 24/08/20
+ *  Time: 10:05 AM
+ *  File Name : InheritanceDemo.java
+ * */
+package inheritance;
+
+public class InheritanceDemo {
+}
