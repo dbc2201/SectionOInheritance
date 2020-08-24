363df95651e54e78bedc0200e59c026d482c1c0c
dbc2201 <divyanshb22@gmail.com>
2020-08-24 10:08:45 +0530

    add the motives for learning Inheritance

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -7,4 +7,12 @@
 package inheritance;
 
 public class InheritanceDemo {
+    // 1. What is Inheritance?
+    // 2. How we can use Inheritance to re-use the code/software that is already developed and tested.
+    // 3. Observe when the 'extends' keyword is used.
+    // 4. We will learn about a `super` class and a `sub` class.
+    // 5. How we can access the members of the super class from the sub class.
+    // 6. Constructor Hierarchy.
+    // 7. Method Overriding, Dynamic Method Dispatch, Abstract Classes & Interfaces.
+    // 8. How to model our programs with all the above concepts.
 }
