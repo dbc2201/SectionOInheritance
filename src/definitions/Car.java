34d27e5778e19b7cf04a27aad8e72d3b89b70a7c
dbc2201 <divyanshb22@gmail.com>
2020-08-24 15:19:52 +0530

    create Car.java

---
+++ src/definitions/Car.java
@@ -0,0 +1,14 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 24/08/20
+ *  Time: 3:16 PM
+ *  File Name : Car.java
+ * */
+package definitions;
+
+public class Car {
+}
+
+
+// The Car IS A TYPE of Vehicle, AND the Car IS ALSO A TYPE of FourWheeler.
+// Java only allows us to extend a single class!
