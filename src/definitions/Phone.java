7ffecb059a4cc44c5db25ed4f888b24eff8478ca
dbc2201 <divyanshb22@gmail.com>
2020-08-24 10:40:01 +0530

    create Phone.java

---
+++ src/definitions/Phone.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 24/08/20
+ *  Time: 10:39 AM
+ *  File Name : Phone.java
+ * */
+package definitions;
+
+public class Phone {
+}
