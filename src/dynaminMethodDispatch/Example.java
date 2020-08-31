be0164e31d0ca08b5fd70765f88000877d1c079f
dbc2201 <divyanshb22@gmail.com>
2020-08-31 15:09:32 +0530

    create Example.java

---
+++ src/dynaminMethodDispatch/Example.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 31/08/20
+ *  Time: 3:09 PM
+ *  File Name : Example.java
+ * */
+package dynaminMethodDispatch;
+
+public class Example {
+}
