3d5453e00045301bdbd423906661d30c1e5f28e8
dbc2201 <divyanshb22@gmail.com>
2020-09-02 15:15:20 +0530

    create Example.java

---
+++ src/programUnits/interfaces/Example.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 02/09/20
+ *  Time: 3:15 PM
+ *  File Name : Example.java
+ * */
+package programUnits.interfaces;
+
+public class Example {
+}
