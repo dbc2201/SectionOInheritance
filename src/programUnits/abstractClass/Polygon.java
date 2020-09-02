d2e8e96887681d7a1d8ea25f65aebc282f64095e
dbc2201 <divyanshb22@gmail.com>
2020-09-02 10:40:02 +0530

    create Polygon.java

---
+++ src/programUnits/abstractClass/Polygon.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 02/09/20
+ *  Time: 10:39 AM
+ *  File Name : Polygon.java
+ * */
+package programUnits.abstractClass;
+
+public class Polygon {
+}
