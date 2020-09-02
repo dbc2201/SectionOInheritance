cdace92cde83698762639af74fce64b68655a48a
dbc2201 <divyanshb22@gmail.com>
2020-09-02 10:36:00 +0530

    create Shape class

---
+++ src/programUnits/abstractClass/Shape.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 02/09/20
+ *  Time: 10:34 AM
+ *  File Name : Shape.java
+ * */
+package programUnits.abstractClass;
+
+public class Shape {
+}
