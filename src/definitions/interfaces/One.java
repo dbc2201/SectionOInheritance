2b228fe859ca3ef93ed523b3051c10d74a06d017
dbc2201 <divyanshb22@gmail.com>
2020-09-02 15:05:19 +0530

    create Interface One

---
+++ src/definitions/interfaces/One.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 02/09/20
+ *  Time: 3:04 PM
+ *  File Name : One.java
+ * */
+package definitions.interfaces;
+
+public interface One {
+}
