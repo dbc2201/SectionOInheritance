325b97464b87c578ad9ddf16cc6d7f84bfc173e6
dbc2201 <divyanshb22@gmail.com>
2020-09-02 15:09:48 +0530

    create a field inside the interface One

--- src/definitions/interfaces/One.java
+++ src/definitions/interfaces/One.java
@@ -7,4 +7,10 @@
 package definitions.interfaces;
 
 public interface One {
+    // For Fields of the Interface.
+    double PI = 3.14;
+    // Because, all the fields declared inside an interface are
+    // are automatically declared as public and final by the compiler.
+
+    // For Methods of the Interface.
 }
