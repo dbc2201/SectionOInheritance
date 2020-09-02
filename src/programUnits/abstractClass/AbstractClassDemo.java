2bd555b2c4ddbc98827499c13901220c5d12e2fc
dbc2201 <divyanshb22@gmail.com>
2020-09-02 10:12:40 +0530

    create AbstractClassDemo.java

---
+++ src/programUnits/abstractClass/AbstractClassDemo.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 02/09/20
+ *  Time: 10:12 AM
+ *  File Name : AbstractClassDemo.java
+ * */
+package programUnits.abstractClass;
+
+public class AbstractClassDemo {
+}
